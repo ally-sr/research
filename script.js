193d9b3b602155580ef4d0cdcf582d0ee537dc2e
Ally Santana <101467403+ally-sr@users.noreply.github.com>
2022-09-29 22:32:00 -0300

    ++

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-const init = () => {
-    const inputUser = document.querySelector('input[type="text"]');
-    const inputPassword = document.querySelector('input[type="password"]');
-    const submitButton = document.querySelector('.btnLogin');
 
-    if (submitButton) {
-        submitButton.addEventListener('click', (event) => {
-            event.preventDefault();
+    function logar() {
+        var login = document.getElementById('login').value;
+        var senha = document.getElementById('senha').value;
 
-            fetch('https://reqres.in/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type':'application/json'
-                },
-                body: JSON.stringify({
-                    email: inputUser.value,
-                    password: inputPassword.value,
-                })
-            }), then((response) => {
-                return response.json();
-            }), then((data) => {
-                console.log(data)
-            }) 
-        })
+        if (login == "admin" && senha == "admin") {
+            aler('Sucesso');
+            location.href = "dashboard.html";
+        }
     }
-}
-
-window.onload = init;
